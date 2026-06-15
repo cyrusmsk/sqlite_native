42018b9e4fc55f4be3342a5a39e35316b8142c07
Sergei Giniatulin <kornburn@gmail.com>
2026-06-15 20:20:16 +0300

    Fix for executable
    sqlite inside executable

--- source/sqlite_native/package.d
+++ source/sqlite_native/package.d
@@ -1,3 +1,12 @@
 module sqlite_native;
 
-public import sqlite3;
+import sqlite3;
+import std.stdio;
+
+void main() {
+    if (sqlite3_initialize() != SQLITE_OK)
+        assert(false, "Failed to initialize sqlite3");
+
+    writeln("sqlite works");
+    sqlite3_shutdown();
+}
